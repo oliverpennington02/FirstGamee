c5c49dfca36b793375b871371c89b9bd79df9d10
oliverpennington02 <98119526+oliverpennington02@users.noreply.github.com>
2023-07-13 22:49:39 -0500

    Generator

---
+++ Assets/WorldGenerator.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TilePrefab
+{
+    public GameObject prefab;
+    [Range(0f, 100f)]
+    public float spawnPercentage;
+}
+
+public class WorldGenerator : MonoBehaviour
+{
+    public int worldWidth = 50;  // Width of the world grid
+    public int worldHeight = 50; // Height of the world grid
+    public int seed = 0; // Seed for random number generation
+    public float noiseScale = 0.1f; // Scale of the Perlin noise
+
+    public TilePrefab[] tilePrefabs; // Array of tile prefabs and spawn percentages
+
+    void Start()
+    {
+        GenerateWorld();
+    }
+
+    void GenerateWorld()
+    {
+        Random.InitState(seed);
+
+        NormalizeSpawnPercentages();
+
+        for (int x = 0; x < worldWidth; x++)
+        {
+            for (int y = 0; y < worldHeight; y++)
+            {
+                float noiseValue = Mathf.PerlinNoise((float)x * noiseScale, (float)y * noiseScale);
+
+                float cumulativeSpawnPercentage = 0f;
+                GameObject selectedPrefab = null;
+
+                foreach (TilePrefab tilePrefab in tilePrefabs)
+                {
+                    cumulativeSpawnPercentage += tilePrefab.spawnPercentage / 100f;
+
+                    if (noiseValue <= cumulativeSpawnPercentage)
+                    {
+                        selectedPrefab = tilePrefab.prefab;
+                        break;
+                    }
+                }
+
+                if (selectedPrefab != null)
+                {
+                    Vector3 position = new Vector3(x, y, 0f);
+                    Instantiate(selectedPrefab, position, Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    void NormalizeSpawnPercentages()
+    {
+        float totalSpawnPercentage = 0f;
+        foreach (TilePrefab tilePrefab in tilePrefabs)
+        {
+            totalSpawnPercentage += tilePrefab.spawnPercentage;
+        }
+
+        if (totalSpawnPercentage == 100f)
+        {
+            return;
+        }
+
+        float adjustmentFactor = 100f / totalSpawnPercentage;
+        foreach (TilePrefab tilePrefab in tilePrefabs)
+        {
+            tilePrefab.spawnPercentage *= adjustmentFactor;
+        }
+    }
+}
